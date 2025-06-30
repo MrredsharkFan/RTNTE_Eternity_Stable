998d153a32fb9e9e672a9ef28f1773f1fae5b3e7
MrRedSharkFan <haychiu2021@gmail.com>
2025-06-30 14:40:01 +0800

    the most barebones shit
    HERE WE GO

--- loop.js
+++ loop.js
@@ -2,20 +2,55 @@
 //2. BAN system adopted from scratch (YES)
 
 //1: 10,X,2 | 10: 10,10,X | 11: 10,X,1,2
+
+localStorage.setItem("username",localStorage.getItem("username")|Math.random());
+var cloud_last_remixed = cloud_last_remixed|-1;
+var cloud_remixes = cloud_remixes|0;
+
 function stack(init,num){
-    prefix = []
+    prefix = [10]
     suffix = []
     jnum = num
-    while (num % 10 == 0){
-        prefix.concat(10)
-        num /= 10
+    while (jnum % 10 == 0){
+        prefix=prefix.concat(10);
+        jnum /= 10
     }
+    jnum--
+    while (jnum != 0){
+        suffix=suffix.concat(jnum % 10+1)
+        jnum -= jnum % 10;
+        jnum /= 10;
+    }
+    return "{"+prefix+","+init+(suffix.length!=0?(","+suffix):"")+"}"
+    
 }
 function num_to_ban(num){
     gay = []
     inum = num
-    while (inum>=1){
-        gay.concat(Math.floor(inum));
-        inum**=(inum % 1) //what the fuck is this???
+    while (inum>1){
+        gay = [Math.floor(inum)].concat(gay);
+        inum*=(inum % 1) //what the fuck is this???
+    }
+    result = (10**(10**inum)).toFixed(3)
+    for (let i in gay){
+        result = stack(result,gay[i])
+    }
+    return result
+}
+
+function Remix(){
+    if (cloud_last_remixed != localStorage.getItem("username")){
+        cloud_last_remixed = localStorage.getItem("username")
+        cloud_remixes++
+    }
+    else {
+        console.warn("fuck you self-remixer nobody likes you fuck you fuck you fuck you")
     }
 }
+
+function mainLoop(){
+    document.getElementById("number").innerHTML = num_to_ban(1.001**cloud_remixes-1);
+    document.getElementById("remix").innerHTML = "Remix <font size=+3>"+cloud_remixes+"</font>"
+}
+
+setInterval(mainLoop,100,1)
